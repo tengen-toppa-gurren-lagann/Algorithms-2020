c3fcd7421d7098e55a640308188160b7a326893c
tengen-toppa-gurren-lagann <un.al@mail.ru>
2020-11-28 19:47:37 +0300

    4th lesson done. Maybe it will be updated with better solution.

--- src/lesson4/Trie.java
+++ src/lesson4/Trie.java
@@ -1,9 +1,10 @@
 package lesson4;
 
-import java.util.*;
-import kotlin.NotImplementedError;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Префиксное дерево для строк
@@ -92,8 +93,76 @@ public class Trie extends AbstractSet<String> implements Set<String> {
     @NotNull
     @Override
     public Iterator<String> iterator() {
-        // TODO
-        throw new NotImplementedError();
+        return  new TrieIterator();
+    }
+
+    public class TrieIterator implements Iterator<String> {
+        private String current;
+        private String next;
+        private StringBuilder charBuffer;
+        private boolean removable;
+        Stack<Iterator<Map.Entry<Character, Node>>> stack; // Стек итераторов детей
+        Set<ConcurrentHashMap.Entry<Character, Node>> childrenSet; // Concurrent из-за ConcurrentModificationException
+
+        private TrieIterator() {
+            childrenSet = new HashSet<>(root.children.entrySet()); // Начинаем проход с детей корня
+            stack = new Stack<>();
+            stack.push(childrenSet.iterator());
+            charBuffer = new StringBuilder();
+            removable = false;
+            current = null;
+            next = nextWord();
+        }
+
+        @Override
+        public boolean hasNext() {
+            return next != null;
+        }
+
+        @Override
+        public String next() {
+            if (!hasNext()) throw new IllegalStateException();
+            removable = true;
+            current = next;
+            next = nextWord();
+            return current;
+        }
+
+        @Override
+        public void remove() {
+            if (!removable) throw new IllegalStateException();
+            boolean removed = Trie.this.remove(current);
+            removable = !removed;
+        }
+
+        private String nextWord() {
+            String word = null;
+            Iterator<Map.Entry<Character, Node>> childrenIterator;
+            childrenIterator = stack.peek(); // Верхний итератор из стека
+            if (childrenIterator == null) throw new IllegalStateException();
+            while (word == null && (stack.size() > 1 || childrenIterator.hasNext())) { // Ищем пока не нашли слово и пока не вернулись к корню, либо у корня остались нерассмотренные дети
+                while (!childrenIterator.hasNext() && charBuffer.length() != 0) { // Если у текущего узла нет детей и мы находимся не в корне поднимаемся наверх
+                    charBuffer.deleteCharAt(charBuffer.length() - 1);
+                    stack.pop();
+                    childrenIterator = stack.peek();
+                    if (childrenIterator == null) throw new IllegalStateException();
+                }
+
+            while (childrenIterator.hasNext()) { // Пока дети есть, собираем строку до тех пор, пока не встретим 0
+                Map.Entry<Character, Node> entry = childrenIterator.next();
+                if (entry.getKey() != (char) 0) {
+                    charBuffer.append(entry.getKey());
+                    childrenSet = new HashSet<>(entry.getValue().children.entrySet()); // Рассматриваем детей узла
+                    childrenIterator = childrenSet.iterator();
+                    stack.push(childrenIterator); // Помещаем в стек итератор следующего элемента
+                } else { // Собрали слово
+                    word = charBuffer.toString();
+                    break;
+                }
+            }
+        }
+            return word;
+        }
     }
 
 }
